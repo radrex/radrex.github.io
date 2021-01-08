8cfa7f531a4d2929475205cdb3ba64a4f1a1e7d6
radrex <radostinv.stoychev@gmail.com>
2021-01-08 23:26:10 +0200

    Add classes for responsive images and buttons

--- static/js/views/Home.js
+++ static/js/views/Home.js
@@ -7,30 +7,20 @@ export default class extends BaseView {
   }
 
   async getHtml() {
+
     return `
       <h1>Hey, I'm Radostin.</h1>
-      <h3>A web developer based in Sofia, Bulgaria.</h3>
-      
-      <a class="btn" href="/work" data-link>My Projects</a>
+      <h2>A web developer based in Sofia, Bulgaria. Specialized in front-end and back-end.</h2>
+      <a class="btn btn-large" href="/work" data-link>My Projects</a>
 
-      <p>
-        <a href="/work" data-link>View recent projects</a>.
-      </p>
-
-
-
-
-
-
-
-
-
-
-
+      <div class="responsive-img-container">
+        <div class="responsive-img"></div>
+      </div>
 
       <ul>
         Attributions:
         <li><a target="_blank" href="https://icons8.com/icons/set/laptop-coding">Laptop Coding icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></li>
+        <li><span>Photo by <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Spiske</a> on <a href="https://unsplash.com/s/photos/code?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></li>
       </ul>
     `;
   }
