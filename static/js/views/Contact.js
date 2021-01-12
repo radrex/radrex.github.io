50de09bce506e90a5614069274214d244ba21833
radrex <radostinv.stoychev@gmail.com>
2021-01-12 21:56:00 +0200

    Finish Contact Form styles

--- static/js/views/Contact.js
+++ static/js/views/Contact.js
@@ -11,23 +11,21 @@ export default class extends BaseView {
       <h1>Contact Me</h1>
       <h2>If you’d like to chat about a project please fill in the form below and I’ll get back within 1-2 days.</h2>
     
-      <section class="window">
-        <section class="buttons">
-          <section class="button red" ></section>
-          <section class="button yellow" ></section>
-          <section class="button green" ></section>
-        </section>
-        <section class="window-body">
-          <form>      
-            <input name="name" type="text" placeholder="Name" />   
+      <section class="mac-window">
+        <div class="buttons">
+          <div class="mac-btn red"></div>
+          <div class="mac-btn yellow"></div>
+          <div class="mac-btn green"></div>
+        </div>
+        <div class="window-body">
+          <form action="" method="POST">   
+            <input name="name" type="text" placeholder="Name" />
             <input name="email" type="text" placeholder="Email" />
             <textarea name="text" placeholder="Message"></textarea>
             
             <input type="submit" value="Send" />
-            <div class="wrapper"></div>
-            
           </form>
-        </section>
+        </div>
       </section>
     `;
   }
