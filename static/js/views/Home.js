37a7472176563208ed62f40a9aec20e52b8ea39a
radrex <radostinv.stoychev@gmail.com>
2021-01-27 20:18:29 +0200

    Add home page skills, footer social icons/links, content for experience page and some styles

--- static/js/views/Home.js
+++ static/js/views/Home.js
@@ -9,18 +9,42 @@ export default class extends BaseView {
   async getHtml() {
     return `
       <h1>Hey, I'm Radostin.</h1>
-      <h2>A web developer based in Sofia, Bulgaria. Specialized in front-end and back-end.</h2>
+      <h2>A web developer based in Sofia, Bulgaria. Specialized in front and back-end.</h2>
+      <h2>I'm really passionate about all sorts of technology. Always looking forward to collaborate on thought-provoking and intriguing projects with enthusiasm to engage in the full process.</h2>
+      
       <a class="btn btn-large" href="/work" data-link>My Projects</a>
 
       <div class="responsive-img-container">
         <img src="../../static/images/markus-spiske-y6HpQzW87Vc-unsplash.jpg" width="3840" height="5760">
       </div>
 
-      <h2>Featured Projects</h2>
+      <section class="favored-tech">
+        <i class="fas fa-layer-group"></i>
+        <h2>Back-End</h2>
+        <ul class="skills">
+          <li>C#</li>
+          <li>MSSQL</li>
+          <li>EF Core</li>
+          <li>ASP.NET Core</li>
+          <li>NodeJS</li>
+          <li>Express</li>
+          <li>Firebase</li>
+        </ul>
+      </section>
+      
+      <section class="favored-tech">
+        <i class="fas fa-drafting-compass"></i>
+        <h2>Front-End</h2>
+        <ul class="skills">
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript</li>
+          <li>Bootstrap</li>
+        </ul>
+      </section>
 
       <ul>
         Attributions:
-        <li><a target="_blank" href="https://icons8.com/icons/set/laptop-coding">Laptop Coding icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></li>
         <li><span>Photo by <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Spiske</a> on <a href="https://unsplash.com/s/photos/code?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></li>
       </ul>
     `;
